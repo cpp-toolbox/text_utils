88f298a59d74d3fd9a4f95e8e14f135d15a82842
ccn <ccn@cuppajoeman.com>
2025-07-28 20:07:24 -0400

    feat: add in multline support to msa

--- text_utils.hpp
+++ text_utils.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <stdexcept>
 
 namespace text_utils {
 
@@ -13,8 +14,54 @@ class MultilineStringAccumulator {
   public:
     template <typename... Args> void add(Args &&...args) {
         std::ostringstream oss;
-        (oss << ... << args); // fold expression (c++17+)
+        (oss << ... << args); // fold expression (C++17+)
         lines_.emplace_back(oss.str());
+    }
+
+    void add_multiline(const std::string &multiline_str) {
+        std::istringstream iss(multiline_str);
+        std::string line;
+        while (std::getline(iss, line)) {
+            lines_.emplace_back(std::move(line));
+        }
+    }
+
+    void insert_line(size_t index, const std::string &line) {
+        if (index > lines_.size()) {
+            throw std::out_of_range("insert_line: index out of range");
+        }
+        lines_.insert(lines_.begin() + index, line);
+    }
+
+    void insert_lines(size_t index, const MultilineStringAccumulator &other) {
+        if (index > lines_.size()) {
+            throw std::out_of_range("insert_lines: index out of range");
+        }
+
+        lines_.insert(lines_.begin() + index, other.lines_.begin(), other.lines_.end());
+    }
+
+    void insert_multiline(size_t index, const std::string &multiline_str) {
+        if (index > lines_.size()) {
+            throw std::out_of_range("insert_multiline: index out of range");
+        }
+
+        std::istringstream iss(multiline_str);
+        std::string line;
+        std::vector<std::string> new_lines;
+
+        while (std::getline(iss, line)) {
+            new_lines.emplace_back(std::move(line));
+        }
+
+        lines_.insert(lines_.begin() + index, new_lines.begin(), new_lines.end());
+    }
+
+    void remove_line(size_t index) {
+        if (index >= lines_.size()) {
+            throw std::out_of_range("remove_line: index out of range");
+        }
+        lines_.erase(lines_.begin() + index);
     }
 
     std::string str() const {
